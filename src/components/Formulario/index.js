db0a6889c190f558e9b6613fded920eb86c78b95
Daniel Carvalho <danielcarrosa@gmail.com>
2024-11-25 20:56:05 -0300

    feat: pegando dados do formulario

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -4,7 +4,7 @@ import CampoTexto from '../CampoTexto';
 import Dropdown from '../Dropdown';
 import './index.css'
 
-const Formulario = () => {
+const Formulario = (props) => {
 
     const times = [
         'Programação',
@@ -23,7 +23,12 @@ const Formulario = () => {
 
     const save = (event) => {
         event.preventDefault()
-        console.log(nome, cargo, imagem , time);
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
     }
 
     return (
