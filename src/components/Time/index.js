5849adbf507219bf7577e6d5bcd2c1257e15d177
Daniel Carvalho <danielcarrosa@gmail.com>
2024-11-27 20:45:09 -0300

    feat: criando as section para os cards

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,9 +1,18 @@
 import './index.css'
 
 const Time = (props) => {
+
+    const sectionCss = {
+        backgroundColor: props.corSecundaria
+    }
+
+    const h3Css = {
+        borderColor: props.corPrimaria
+    }
+
     return (
-        <section className='time'>
-            <h3>{props.nome}</h3>
+        <section className='time' style={sectionCss}>
+            <h3 style={h3Css}>{props.nome}</h3>
         </section>
     )
 }
