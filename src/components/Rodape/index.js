d740ed90b6dfc73f8b3e82ecf38b9fb8e07da131
Daniel Carvalho <danielcarrosa@gmail.com>
2024-11-28 23:13:07 -0300

    feat: component button

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -4,6 +4,30 @@ const Rodape = ( ) => {
     return (
         <div className='rodape'>
             <footer>
+                <ul>
+                    <li>
+                        <a href="https://www.facebook.com/login/?locale=pt_BR">
+                            <i>
+                                <img src="/imagens/fb.png" alt="Facebook"/>
+                            </i>
+                        </a>                      
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/">
+                            <i>
+                                <img src="/imagens/ig.png" alt="Instagram"/>
+                            </i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='https://x.com/?lang=pt-br&mx=2'>
+                            <i>
+                                <img src="/imagens/tw.png" alt="Twitter"/>
+                            </i>
+                        </a>
+                    </li>
+                </ul>
+
                 <img src="/imagens/fundo.png" alt="Rodapé."/>
             </footer>
         </div>
